fd0cfab52c2e0aa3357cb96858a263ef821cf41d
Abhinav Mishra <abhinavmishraise@gmail.com>
2021-11-28 11:06:55 +0530

    db cred changes

--- database/dbcredentials.js
+++ database/dbcredentials.js
@@ -1,8 +1,8 @@
 const credentials = {
-    host : "localhost",
-    user : "postgres",
-    password : "12345",
-    database : "firstExpressApp"
+    host : "ec2-23-23-219-25.compute-1.amazonaws.com",
+    user : "tfcwjjldbufezt",
+    password : "3a3bc6c21e55c8309b61ad796c32be02bd341e1a52c5b98e8c703c4c6e390aab",
+    database : "d1svklad9cfb55"
 };
 
 module.exports = credentials;
