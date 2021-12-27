e2549c3dbfdd50e85f53cc290fef98601f531f3a
Abhinav Mishra <abhinavmishraise@gmail.com>
2021-12-27 09:06:00 +0530

    boolean to string response

--- controller/customerController.js
+++ controller/customerController.js
@@ -41,7 +41,7 @@ router.post('/new', function(req, res){
 
     }).then(function (article) {
         if (article) {
-            res.status(200).send({message: true});
+            res.status(200).send({message: "true"});
         } else {
             res.status(400).send({message:'Error creating user'});
         }
