951a4369c2d8b1be97e6fa375d3997175708b36d
Abhinav Mishra <abhinavmishraise@gmail.com>
2022-01-02 11:53:55 +0530

    bug fix cors code

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require('cors')
+var cors = require('cors');
 
 var adminController = require('./controller/adminController');
 var orderController = require('./controller/orderController');
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors);
+app.use(cors());
 
 app.use('/admin', adminController);
 app.use('/customer', customerController);
