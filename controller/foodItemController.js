72a20b8e49f6c303e45863fd68531b96456a281f
Abhinav Mishra <abhinavmishraise@gmail.com>
2022-01-02 21:28:22 +0530

    food item controller auth and test

--- controller/foodItemController.js
+++ controller/foodItemController.js
@@ -1,41 +1,89 @@
 var express = require('express');
 var router = express.Router();
 const dbconnect = require('../database/dbconnect');
+const Sequelize = require("sequelize");
 
+async function validate(req){
+    if (Object.keys(req.query).length === 0)
+        return 1;
+    const token = req.query.token;
+    let querySelect = 'select a.id, a.email email, a.mobile mobile, a.password, a.user, a.token from admins a where token = :token union all select c.id, c.email email, c.mobile mobile, c.password, c.user, c.token from customers c where token = :token;';
+
+    let result = await dbconnect.connection.query(querySelect, {
+        replacements: {token: token},
+        type: Sequelize.QueryTypes.SELECT
+    });
+    if (result.length === 0){
+        return null;
+    }
+    else
+        return result[0];
+}
 
 router.get('/', async function (req, res) {
-    dbconnect.Customer.findAll({
-        where: {
-            token: req.query.token
-        }
-    }).then(async function (customer) {
-        if (customer.length !== 0) {
-            const items = await dbconnect.FoodItem.findAll();
-            res.status(200).send(items);
-        } else {
-            res.status(400).send(false);
-        }
-    });
+
+    let user = await validate(req);
+
+    if (user != null) {
+        const items = await dbconnect.FoodItem.findAll();
+        res.status(200).send(items);
+    } else {
+        res.status(400).send(false);
+    }
+});
+
+router.post('/update', async function(req, res){
+
+    let user = await validate(req);
+
+    if (user != null) {
+        return dbconnect.FoodItem.update({
+            name: req.body.name,
+            type: req.body.type,
+            price: req.body.price,
+            description: req.body.description,
+            rating: req.body.rating,
+            url: req.body.url
+        },{
+            where: {
+                id: req.body.id
+            }
+        }).then(function (food) {
+            if (food) {
+                res.status(200).send(food);
+            } else {
+                res.status(400).send('Error in inserting new record');
+            }
+        });
+    } else {
+        res.status(400).send(false);
+    }
 
 });
 
-router.post('/new', function(req, res){
+router.post('/new', async function(req, res){
 
-    return dbconnect.FoodItem.create({
-        name: req.body.name,
-        type: req.body.type,
-        price: req.body.price,
-        description: req.body.description,
-        rating: req.body.rating,
-        url: req.body.url
+    let user = await validate(req);
 
-    }).then(function (article) {
-        if (article) {
-            res.send(article);
-        } else {
-            res.status(400).send('Error in inserting new record');
-        }
-    });
+    if (user != null) {
+        return dbconnect.FoodItem.create({
+            name: req.body.name,
+            type: req.body.type,
+            price: req.body.price,
+            description: req.body.description,
+            rating: req.body.rating,
+            url: req.body.url
+
+        }).then(function (article) {
+            if (article) {
+                res.status(200).send(article);
+            } else {
+                res.status(400).send('Error in inserting new record');
+            }
+        });
+    } else {
+        res.status(400).send(false);
+    }
 });
 
 module.exports = router;
--- test/testScript.js
+++ test/testScript.js
@@ -1,41 +1,62 @@
 const assert = require('assert');
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../app');
+let should = chai.should();
 
-// We can group similar tests inside a describe block
-describe("Simple Calculations", () => {
+
+chai.use(chaiHttp);
+
+describe("Food Items", () => {
+
     before(() => {
-        console.log( "This part executes once before all tests" );
+        console.log( "Running tests for food item APIs" );
     });
 
     after(() => {
-        console.log( "This part executes once after all tests" );
+        console.log( "Finished running tests for food item APIs" );
     });
 
-    // We can add nested blocks for different tests
-    describe( "Test1", () => {
-        beforeEach(() => {
-            console.log( "executes before every test" );
-        });
-
-        it("Is returning 5 when adding 2 + 3", () => {
-            assert.equal(2 + 3, 5);
-        });
-
-        it("Is returning 6 when multiplying 2 * 3", () => {
-            assert.equal(2*3, 6);
+    describe('/GET foodItem', () => {
+        it('it should GET all the foodItems', (done) => {
+            chai.request(server)
+                .get('/foodItem?token=de0886678687731f26616f6b18a5ccd5c55058a0dd0ea03413456a99e652')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+                    // res.body.length.should.be.eql(0);
+                    done();
+                });
         });
     });
 
-    describe("Test2", () => {
-        beforeEach(() => {
-            console.log( "executes before every test" );
+
+    describe('/POST foodItem', () => {
+        it('it should create a food item', (done) => {
+            let foodItem = {
+                "name": "Chicken Soup",
+                "type": "non-veg",
+                "price": 200,
+                "description": "Chicken curry is a dish originating from the Indian subcontinent. It is common in the Indian subcontinent, Southeast Asia, Great Britain, and the Caribbean.",
+                "rating": "5",
+                "url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.cookingclassy.com%2Fchicken-curry%2F&psig=AOvVaw2k0MN1rj5oVs93mOt1usIF&ust=1638187928227000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNjPqd6Du_QCFQAAAAAdAAAAABAD"
+            }
+            chai.request(server)
+                .post('/foodItem/new?token=de0886678687731f26616f6b18a5ccd5c55058a0dd0ea03413456a99e652')
+                .send(foodItem)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('id');
+                    res.body.type.should.be.eql(foodItem.type);
+                    res.body.name.should.be.eql(foodItem.name);
+                    // res.body.errors.should.have.property('pages');
+                    // res.body.errors.pages.should.have.property('kind').eql('required');
+                    done();
+                });
         });
 
-        it("Is returning 4 when adding 2 + 3", () => {
-            assert.equal(2 + 3, 4);
-        });
-
-        it("Is returning 8 when multiplying 2 * 4", () => {
-            assert.equal(2*4, 8);
-        });
     });
+
 });
+
